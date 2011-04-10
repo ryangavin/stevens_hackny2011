44f1f96627d1cedbfe8fe454620d59bb64962ec2
Alex Shulman <ajshulman@gmail.com>
2011-04-10 01:21:41 -0400

    fixed login Please enter the commit message for your changes. Lines starting

--- app/views.py
+++ app/views.py
@@ -7,9 +7,9 @@ application.
 from google.appengine.api import mail
 import hyper
 from flask import Module, url_for, render_template, request, redirect
-from models import Todo
+from models import Todo, User
 from forms import TodoForm, EmailForm
-
+from decorator import login_required
 import hunch
 import settings
 
@@ -26,12 +26,15 @@ def test():
     data = hunch.get_recommendations()
     return data
 
-@views.route('/login/')
+@views.route('/login/', methods=['POST', 'GET'])
+@login_required
 def login():
 	"""Handle login response from hunch"""
 	key = request.args.get('auth_token_key')
-	id = request.args.get('user_id')
-	return render_template('login.html',key=key, id=id)
+	user_id = request.args.get('user_id')
+	user = User(user_id=user_id, auth_token_key=key)
+	user.put()
+	return render_template('login.html',key=key, userid=user_id)
 
 
 @views.route('/todo/')
@@ -42,6 +45,10 @@ def todo_list():
     return render_template('todo.html', form=form,
             todos=todos)
 
+@login_required
+@views.route('/auth')
+def loggedin():
+	return render_template('login.html')
 
 @views.route('/todo/add', methods=["POST"])
 def add_todo():
@@ -54,6 +61,7 @@ def add_todo():
 
 @views.route('/hyper')
 def hyper_foo():
+
 	return hyper.getHtml()
 
 
