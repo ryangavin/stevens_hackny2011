d84da4d56215510a0599ad0cc7bb7910f15dfcc3
Alex Shulman <ajshulman@gmail.com>
2011-04-09 21:11:48 -0400

    changed settings globals

--- app/settings.py
+++ app/settings.py
@@ -8,4 +8,12 @@ Generate good secret keys:  http://flask.pocoo.org/docs/quickstart/#sessions
 
 """
 
-SECRET_KEY = 'this_obviously_should_be_changed_using_os.urandom()'
+SECRET_KEY = '0CPn5zl908l4TYV44H5913PiYqA855k1'
+HUNCH_ID = '3145664'
+HUNCH_SECRET = 'ac1dfa282b4f6ffc1dd061b5964ff92423a72f9b'
+HYPE_CLIENT_ID = 'Nr3IYmc7W04UubgMxLinOufTBJLhNQYJwO4pMA2N'
+HYPE_CLIENT_SECRET = 'F8tCiOOwnIZZmB6snMe8MwjffEJsrgHoK1YcGN1E'
+HYPE_REQUEST_URL = 'http://hyperpublic.com/oauth/request_token' 
+HYPE_ACCESS_URL = 'http://hyperpublic.com/oauth/access_token' 
+HYPE_AUTHORIZE_URL = 'http://hyperpublic.com/oauth/authorize'
+
