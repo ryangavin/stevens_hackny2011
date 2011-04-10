0ea1f99f8157b8d285a1f5510abd3145960a04bd
Simon Sidhom <ssidhom@stevens.edu>
2011-04-10 08:07:27 -0400

    asdfasdflwejfl;wf

--- app/main.py
+++ app/main.py
@@ -7,17 +7,30 @@ import settings
 from flask import request
 import simplejson as json
 from google.appengine.api import urlfetch
+
+
+def match_numbers(deals, numbers):
+	matches = []
+	for each in deals:
+		for num in numbers:
+			if each[1] == num :
+				matches.append(num)
+	return matches
+	
 def main():
 
 	lat = 40.74
 	lon = -74.0
 	rad = 300
+	
+	ret = [lat, lon]
+
 	position = geo.get_pos(request.remote_addr)
 #	lat = position[0]
 #	lon = position[1]
 	urlfetch.fetch('http://hunch.com/authorize/v1/?appid=3145664')
-	ret = "The map below shows places that Dealocos thinks your going to like :) <BR/>"
-	ret = ret + "Below the map is a list of deals in your area that Dealocos thinks your going to like<BR/>"
+#	ret = "The map below shows places that Dealocos thinks your going to like :) <BR/>"
+#	ret = ret + "Below the map is a list of deals in your area that Dealocos thinks your going to like<BR/>"
 	tags = []
 	ts = hunch.get_tags()
 	for tagLS in ts:
@@ -25,12 +38,17 @@ def main():
 			tags.append(tag)
 			
 	hypRet = hyper.getHtml(tags, request)
-	ret = ret + hypRet[0] +"<BR/>"
-	ret = ret + str(yipit_attempt.request_deals(lat, lon , rad , tags))+"<BR/>"
+	yipRet = yipit_attempt.request_deals(lat, lon , rad , tags)
 	
+#	ret = ret + hypRet[0] +"<BR/>"
+#	ret = ret + str(yipit_attempt.request_deals(lat, lon , rad , tags))+"<BR/>"
+	markers = []
 	for hypNum in hypRet[1]:
-		if hypNum == yipNum:
-			latLon = place["locations"][0]["lat"] + "," + place["locations"][0]["lon"]
-			markers.append(latLon)
+		for yipNum in yipRet:
+			if hypNum == yipNum[1]:
+				latLon = hypNum[2] + "," + hypNum[3]
+				markers.append(latLon)
 	string = "%7C".join(markers)
-	return ret
+	ret.append(string)
+#	ret = ret + '<img src="http://maps.google.com/maps/api/staticmap?center='+str(lat)+','+str(lon)+'&zoom=12&size=400x400&sensor=false&markers=color:blue%7Clabel:H%7C'+ string+'" /><BR/>'
+	return render_template('index.html', ret)
--- app/yipit_attempt.py
+++ app/yipit_attempt.py
@@ -1,37 +1,31 @@
-import json
+import simplejson as json
 import urllib
+from google.appengine.api import urlfetch
 
 #base might be made global
 #lat, lon, rad are integers
 #returns all deals in that radius
 #tag is a list of tags
-def request_deals(lat, lon, rad, tag):
+def request_deals(lat, lon, rad, tags):
 	tag_str = ''
 	for each in tags:
 		tag_str += each.lower()+','
 	tag_str += 'P3'
-	base = 'http://api.yipit.com/v1/deals/?key=uqfEMBjPGCyfcxNV&'
+	base = 'http://api.yipit.com/v1/deals/?key=uqfEMBjPGCyfcxNV'
 	out_deals = []
-	pagedata = urllib.urlopen(base+'&lat='+str(lat)+"&lon="+str(lon)+'&radius='+str(rad)+'&tag='+tag_str).read()
-	pagedata = json.loads(pagedata)
-	if pagedata['meta']['code'] == 200:
+	pagedata = urlfetch.fetch(base+'&lat='+str(lat)+"&lon="+str(lon)+'&radius='+str(rad)+'&tag='+tag_str)
+	if pagedata.status_code == 200:
+		pagedata = json.loads(pagedata.contents)
 		for each in pagedata['response']['deals']:
 			out_deals.append([each['title'] , each['business']['locations'][0]['phone'] ])
 	else:
 		print 'fucking error man'
 	return out_deals
 
-def match_numbers(deals, numbers):
-	matches = []
-	for each in deals:
-		for num in numbers:
-			if each[1] == num :
-				matches.append(num)
-	return matches
 
-dealstuff= request_deals()
-for each in dealstuff:
-	print each
+#dealstuff= request_deals()
+#for each in dealstuff:
+#	print each
 
 
 
