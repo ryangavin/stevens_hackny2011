abbeef2117101fb2c70788cb7c094de236c5596f
Ryan Gavin <ryan.michael.gavin@gmail.com>
2011-04-10 09:26:03 -0400

    im tired as fuck

--- app/hunch.py
+++ app/hunch.py
@@ -1,9 +1,9 @@
 import settings
-import urllib
+from urllib import urlopen
 import simplejson as json
 
 def get_tags():
-    data = urllib.urlopen('http://api.hunch.com/api/v1/get-recommendations/?auth_token=84362eac251bbe9b734af8ed1949a6c03623d4cb&topic_ids=cat_eat-drink&lat=40.74&lng=-74.00&radius=1.5&limit=5').read()
+    data = urlopen('http://api.hunch.com/api/v1/get-recommendations/?auth_token=84362eac251bbe9b734af8ed1949a6c03623d4cb&topic_ids=cat_eat-drink&lat=40.74&lng=-74.00&radius=1.5&limit=5').read()
     data = json.loads(data)
     data = data['recommendations']
 
